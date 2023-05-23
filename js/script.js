dc1834bdac16650e3d9d8088d15bcf90abdf1e1d
Christelmarita <christelmarita.oe@gmail.com>
2023-05-23 18:01:24 +0200

    scroll

--- js/script.js
+++ js/script.js
@@ -108,7 +108,7 @@ async function main() {
           showSlide(slideIndex);
         }
         prevButton.style.display = slideIndex > 0 ? "block" : "none";
-        resultContainer.scrollIntoView({ behavior: "smooth", block: "start"});
+        resultContainer.scrollIntoView({ behavior: "smooth", block: "center"});
         };
   
     const prevSlide = () => {
