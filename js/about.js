3ff023f893ac7b79e0e61b164dad90a6a345c917
Christelmarita <christelmarita.oe@gmail.com>
2023-05-25 22:13:18 +0200

    animation added

--- js/about.js
+++ js/about.js
@@ -5,3 +5,23 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+function fadeInOnScroll() {
+    var breakDiv = document.querySelector('.break');
+    var breakDivOffset = breakDiv.offsetTop;
+    var windowHeight = window.innerHeight;
+
+    function handleScroll() {
+        var scrollTop = window.scrollY;
+
+        if (scrollTop + (windowHeight * 0.8) > breakDivOffset) {
+            breakDiv.classList.add('visible');
+        } else {
+            breakDiv.classList.remove('visible');
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+}
+
+fadeInOnScroll();
